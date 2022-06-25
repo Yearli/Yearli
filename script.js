a29eca7fc8a07d63576e2d89afd12cd210b29014
SoumyaCMC <soumya133.ranjan@gmail.com>
2022-06-25 12:09:05 +0530

    Fully Built Email Form

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const btn = document.getElementById('button');
-const form = document.getElementById('contact-form');
+const form = document.getElementById('form');
  form.addEventListener('submit', function(event) {
    event.preventDefault();
    btn.value = 'Sending...';
    const serviceID = 'default_service';
    const templateID = 'template_3iy1kd5';
+   console.log('Sending');
    emailjs.sendForm(serviceID, templateID, this)
    .then(() => {
      btn.value = 'Send Email';
