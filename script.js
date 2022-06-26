34745e15a3b5252730de604559d0ecaab72521b0
RahulGautamSingh <rahultesnik@gmail.com>
2022-06-26 18:38:25 +0530

    update design

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const btn = document.getElementById('button');
-const form = document.getElementById('form');
- form.addEventListener('submit', function(event) {
-   event.preventDefault();
-   btn.value = 'Sending...';
-   const serviceID = 'default_service';
-   const templateID = 'template_3iy1kd5';
-   emailjs.sendForm(serviceID, templateID, this)
-   .then(() => {
-     btn.value = 'Send Email';
-     alert('Your Message Has Been Sent');
-   }, (err) => {
-     btn.value = 'Send Email';
-     alert(JSON.stringify(err));
-   });
-});
+function mail(){
+  window.location = "mailto:rahultesnik@gmail.com"
+}
